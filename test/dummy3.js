3e450e5ea3be2c5e76fd90a2e66c1a621e38c72e
Adam Gruber <adam.gruber@dowjones.com>
2014-07-14 17:42:27 -0400

    add more nesting for testing

--- test/dummy3.js
+++ test/dummy3.js
@@ -5,7 +5,16 @@ new fiveby(function (browser) {
   return describe('Dummy Test 3', function () {
 
     describe("i'm nested!", function() {
-      it('does something', function (done) {
+
+      describe("i'm nested 2!", function() {
+        it('does something while nested 2', function (done) {
+          true.should.be.ok;
+          done();
+        });
+      })    
+
+
+      it('does something while nested', function (done) {
         true.should.be.ok;
         done();
       });
