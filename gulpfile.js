b8a54f11477d6412ffd182af0a865f134abd5fe3
Adam Gruber <adam.gruber@dowjones.com>
2014-07-21 10:53:47 -0400

    add no-exit option

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 //TODO: feeding in configs, logins, selenium servers, browsers.. tests should do this so they can run in isolation but that's a lot more maintenance...
 var gulp = require('gulp');
 var mocha = require('gulp-spawn-mocha');
-var options = {reporter: 'mochawesome', timeout: 30000, slow: 1};
+var options = {reporter: 'mochawesome', timeout: 30000, slow: 1, "no-exit": true};
 
 
 gulp.task('test', function () {
