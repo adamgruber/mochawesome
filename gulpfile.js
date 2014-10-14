178a2786813c64871d5a5f096c4689214c642130
Adam Gruber <adam.gruber@dowjones.com>
2014-10-14 16:53:16 -0400

    gulp fiveby will ignore base test file

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var options = {reporter: 'mochawesome', timeout: 30000, slow: 1, "no-exit": true
 
 
 gulp.task('fiveby', function () {
-  return gulp.src('test/**/*.js')
+  return gulp.src('test/**/*.js', '!test/test.js')
     .pipe(mocha(options))
     .on("error", console.warn.bind(console));
 });
