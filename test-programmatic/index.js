d4aaf1410ccf96a4952431bbeee9aebcc6a77706
Adam Gruber <adamg@aweber.com>
2017-03-30 17:02:52 -0400

    ability to test mocha programmatically

---
+++ test-programmatic/index.js
@@ -0,0 +1,29 @@
+const Mocha = require('mocha');
+const fs = require('fs');
+const path = require('path');
+require('should');
+require('babel-register');
+
+// Instantiate a Mocha instance.
+const mocha = new Mocha({
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportFilename: 'adam',
+    timestamp: 'dd yyyy'
+  }
+});
+
+const testDir = path.resolve(__dirname, '../test-functional');
+
+// Add each .js file to the mocha instance
+fs.readdirSync(testDir)
+  // .filter(file => file.substr(-3) === '.js')
+  .filter(file => file === 'test.js')
+  .forEach(file => mocha.addFile(path.join(testDir, file)));
+
+// Run the tests.
+mocha.run(failures => {
+  process.on('exit', () => {
+    process.exit(failures);  // exit with non-zero status if there were failures
+  });
+});
