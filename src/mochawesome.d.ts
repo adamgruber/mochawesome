e3f2c09ada3d8886968e4bce3701c5f77c9f7547
Adam Gruber <agruber@getguru.com>
2021-08-24 13:50:45 -0400

    Add mochawesome declaration file
    Includes ReporterOptions, Options, and Config to start

---
+++ src/mochawesome.d.ts
@@ -0,0 +1,30 @@
+// Type definitions for mochawesome 7.0
+// Project: https://github.com/adamgruber/mochawesome
+// Definitions by: Adam Gruber <https://adamgruber.com>
+
+declare namespace Mochawesome {
+  interface ReporterOptions {
+    'no-code': boolean;
+    code: boolean;
+    consoleReporter: string;
+    html: boolean;
+    json: boolean;
+    quiet: boolean;
+    reportFilename: string;
+  }
+
+  interface Options {
+    inlineDiffs?: boolean;
+    reporterOptions?: Partial<ReporterOptions>;
+  }
+
+  interface Config {
+    code: boolean;
+    consoleReporter: string;
+    quiet: boolean;
+    reportFilename: string;
+    saveHtml: boolean;
+    saveJson: boolean;
+    useInlineDiffs: boolean;
+  }
+}
