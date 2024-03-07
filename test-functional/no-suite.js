797a61035e1d71944970700bb4fa711c55a7cf91
Matthew Prestifilippo <m.prestifilippo@gmail.com>
2024-03-07 16:34:28 -0500

    Make it better

--- test-functional/no-suite.js
+++
@@ -1,7 +0,0 @@
-it('should pass', () => {
-  (1 + 1).should.equal(2);
-});
-
-it('shall not pass', () => {
-  (1 + 12).should.equal(2);
-});
--- test-parallel/test-2.js
+++
@@ -1,8 +0,0 @@
-describe('Parallel Test Suite #2', () => {
-  const attempts = [1, 2, 3, 4, 5];
-  attempts.forEach(attempt => {
-    it(`should take 1 second of time ${attempt}/${attempts.length}`, done => {
-      setTimeout(done, 1000);
-    });
-  });
-});
